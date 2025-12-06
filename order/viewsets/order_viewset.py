b7cf9febfefbc8c89022931c783d38d0c0650c7a
monosodrac <monosodrac@gmail.com>
2025-12-06 16:13:53 -0300

    feat: configure authentication in order

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from order.models import Order 
 from order.serializers import OrderSerializer
@@ -8,3 +10,6 @@ class OrderViewSet(ModelViewSet):
 
     serializer_class = OrderSerializer
     queryset = Order.objects.all().order_by('id')
+
+    authenticantion_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated]
--- product/tests/test_viewsets/test_product_viewset.py
+++ product/tests/test_viewsets/test_product_viewset.py
@@ -13,8 +13,8 @@ class TestProductViewSet(APITestCase):
 
     def setUp(self):
         self.user = UserFactory()
-        token = Token.objects.create(user=self.user) #added
-        token.save() #added
+        token = Token.objects.create(user=self.user)
+        token.save()
 
         self.product = ProductFactory(
             title='pro controller',
@@ -22,8 +22,6 @@ class TestProductViewSet(APITestCase):
         )
 
     def test_get_all_product(self):
-        token = Token.objects.get(user__username=self.user.username) #added
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key) #added
         response = self.client.get(
             reverse('product-list', kwargs={'version': 'v1'})
         )
